b31f4c6750cc079e79e62d618d8d36b8e05796a4
prahinstas555-svg <prahinstas555@gmail.com>
2026-08-26 12:44:46 +0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import './styles/global.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="/mama-lena-webapp">
+    <BrowserRouter basename="/mamalena-bot">
       <App />
     </BrowserRouter>
   </React.StrictMode>
